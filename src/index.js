b3c79f446cec68685a4c8b39b39f7602b0597e64
lssport <111240117+lssport@users.noreply.github.com>
2022-12-04 06:38:17 +0700

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
-import css from './style.css';
-import  reportwebVitals from "./reportwebVitals";
+import css from "./style.cs";
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
@@ -12,4 +11,3 @@ root.render(
     <App />
   </StrictMode>
 );
-reportwebVitals();
