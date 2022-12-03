69f016fec90bfcf4c9b73bf5d36661eea3ddb080
lssport <111240117+lssport@users.noreply.github.com>
2022-12-04 06:34:48 +0700

    1 file modified

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
 import css from './style.css';
-import  reportwebVitals from "./reportwebVital";
+import  reportwebVitals from "./reportwebVitals";
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
