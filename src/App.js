dbd3b4113c6f6a319419f1c03718b2d605592e92
lssport <111240117+lssport@users.noreply.github.com>
2022-12-03 23:59:43 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -22,22 +22,21 @@ render() {
         <div className='card-wrapper p-4'>
           <div className='icon-wrapper mb-1'>
             <i class="bi bi-wallet2"></i>
-  
             <span className='title-sm'>pemasukan</span>
             <h3 className='fw-bold'>Rp.2.000.000,-</h3>
             <span className='title-sm text-ungu fw-bold'>50</span>
             <span className='title-sm'>transaksi</span>
             <div className='row'>
               <div className='col-6'>
-<div className='card-wrapoer p-4'>
+<div className='card-wrapper p-4'>
   <div className='icon-wrapper mb-1'>
-    <i class="bi bi-wallet2"></i>
+    <i class="bi bi-cash-stack"></i>
     <span className='title-sm'>pengeluaran</span>
     <h3 className='fw-bold'>Rp.1.000.000,-</h3>
 <span className='title-sm text-pink fw-bold'>50</span>
 <span className='title-sm'>transaksi</span>
             <div className='row mt-5'>
-              <div className='col-12'>
+              <div className='col-12 d-flex justify-content-between align-items-center'>
                 <h4>ringkasan transaksi</h4>
                 <div className='wrapper-button'>
                   <button className='button btn-ungu px-3 py-2 me-2'>pemasukan<i class="bi bi-plu-circle-fill"></i></button>
@@ -50,7 +49,7 @@ render() {
   <div className='transaction ms-2 d-flex-column'>
     <h6>Menerima Gaji</h6>
     <span className='title-sm'>1 july 2022</span>
-    <h5 className='money'>Rp.1.000.000</h5>
+    <h5 className='text-money in'>Rp.1.000.000</h5>
     </div>
     </div>
     </div>
